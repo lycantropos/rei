8d05d2e16f6276a95ac9f37334f043ae4554a3a3
Azat Ibrakov <azatibrakov@gmail.com>
2021-03-23 11:47:00 +0300

    Add bound 'Rune' class

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <pybind11/pybind11.h>
 #include <re2/re2.h>
+#include <util/utf.h>
 
 #include <string>
 
@@ -9,15 +10,40 @@ namespace py = pybind11;
 #define C_STR_HELPER(a) #a
 #define C_STR(a) C_STR_HELPER(a)
 #define EXPRESSION_NAME "Expression"
+#define RUNE_NAME "Rune"
 #ifndef VERSION_INFO
 #define VERSION_INFO "dev"
 #endif
 
 using Expression = re2::RE2;
+class Rune {
+ public:
+  Rune(const py::bytes& characters) {
+    re2::chartorune(&_raw, std::string(characters).c_str());
+  }
+
+  operator bool() const { return _raw != re2::Runeerror; }
+
+  operator py::str() const {
+    char* c_string = new char[re2::UTFmax]();
+    int length = re2::runetochar(c_string, &_raw);
+    py::str result(c_string, length);
+    delete[] c_string;
+    return result;
+  }
+
+ private:
+  re2::Rune _raw;
+};
 
 PYBIND11_MODULE(MODULE_NAME, m) {
   m.doc() = R"pbdoc(Python binding of `re2` C++ library.)pbdoc";
   m.attr("__version__") = C_STR(VERSION_INFO);
+
+  py::class_<Rune>(m, RUNE_NAME)
+      .def(py::init<const py::bytes&>(), py::arg("characters"))
+      .def("__bool__", &Rune::operator bool)
+      .def("__str__", &Rune::operator py::str);
 
   py::class_<Expression>(m, EXPRESSION_NAME)
       .def(py::init([](const std::string& pattern) {
