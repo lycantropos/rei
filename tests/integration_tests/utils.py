6101292e06ae06daf977efafd7decbae25b59c1c
Azat Ibrakov <azatibrakov@gmail.com>
2021-11-01 11:58:40 +0300

    Complete tests utilities

--- tests/integration_tests/utils.py
+++ tests/integration_tests/utils.py
@@ -12,6 +12,7 @@ def are_bound_ported_runes_equivalent(bound: BoundRune,
 
 def are_enumerations_equivalent(left: Type[Enum], right: Type[Enum]) -> bool:
     left_members, right_members = left.__members__, right.__members__
-    return (left_members.keys() == right_members.keys()
+    return (left.__name__ == right.__name__
+            and left_members.keys() == right_members.keys()
             and all(left_value == right_members[name]
                     for name, left_value in left_members.items()))
