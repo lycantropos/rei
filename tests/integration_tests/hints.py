ce103a057b31375387454e01eddbfa21f7b0a30b
Azat Ibrakov <azatibrakov@gmail.com>
2021-11-03 19:49:43 +0300

    Complete strategies

---
+++ tests/integration_tests/hints.py
@@ -0,0 +1,6 @@
+from typing import Tuple
+
+from tests.binding_tests.hints import BoundStatusCode
+from tests.port_tests.hints import PortedStatusCode
+
+BoundPortedStatusCodePair = Tuple[BoundStatusCode, PortedStatusCode]
--- tests/integration_tests/utils.py
+++ tests/integration_tests/utils.py
@@ -1,8 +1,11 @@
 from enum import Enum
 from typing import Type
 
-from tests.binding_tests.hints import BoundRune
-from tests.port_tests.hints import PortedRune
+from tests.binding_tests.hints import (BoundRune,
+                                       BoundStatusCode)
+from tests.integration_tests.hints import BoundPortedStatusCodePair
+from tests.port_tests.hints import (PortedRune,
+                                    PortedStatusCode)
 
 
 def are_bound_ported_runes_equivalent(bound: BoundRune,
@@ -16,3 +19,8 @@ def are_enumerations_equivalent(left: Type[Enum], right: Type[Enum]) -> bool:
             and left_members.keys() == right_members.keys()
             and all(left_value == right_members[name]
                     for name, left_value in left_members.items()))
+
+
+def to_bound_with_ported_status_code_pairs(value: int
+                                           ) -> BoundPortedStatusCodePair:
+    return BoundStatusCode(value), PortedStatusCode(value)
