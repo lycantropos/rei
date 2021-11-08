e57f00a3e4a2b4050d49aebcb368d2de42fa7c4f
Azat Ibrakov <azatibrakov@gmail.com>
2021-11-08 14:38:14 +0300

    Complete ported 'Status.__init__' magic method

--- rei/status.py
+++ rei/status.py
@@ -6,7 +6,9 @@ from .status_code import StatusCode
 class Status:
     __slots__ = 'code', 'error_arg'
 
-    def __init__(self, code: StatusCode, error_arg: str = '') -> None:
+    def __init__(self,
+                 code: StatusCode = StatusCode.SUCCESS,
+                 error_arg: str = '') -> None:
         self.code = code
         self.error_arg = error_arg
 
