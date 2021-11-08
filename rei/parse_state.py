aa76a3f3150e7a37678fb395aac121b0c2f4c73b
Azat Ibrakov <azatibrakov@gmail.com>
2021-11-08 14:38:38 +0300

    Add ported 'ParseState' class

---
+++ rei/parse_state.py
@@ -0,0 +1,27 @@
+from reprit.base import generate_repr
+
+from .parse_flag import ParseFlag
+from .status import Status
+
+
+class ParseState:
+    __slots__ = '_flag', '_pattern', '_status'
+
+    def __init__(self, flag: ParseFlag, pattern: str) -> None:
+        self._flag = flag
+        self._pattern = pattern
+        self._status = Status()
+
+    __repr__ = generate_repr(__init__)
+
+    @property
+    def flag(self) -> ParseFlag:
+        return self._flag
+
+    @property
+    def pattern(self) -> str:
+        return self._pattern
+
+    @property
+    def status(self) -> Status:
+        return self._status
