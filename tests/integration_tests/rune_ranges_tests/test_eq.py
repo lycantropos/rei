ea128af799ec6d82e413354bef842f8b5b1a32e4
Azat Ibrakov <azatibrakov@gmail.com>
2021-11-12 01:00:37 +0300

    Add test for bound & ported 'RuneRange.__eq__' magic methods

---
+++ tests/integration_tests/rune_ranges_tests/test_eq.py
@@ -0,0 +1,15 @@
+from hypothesis import given
+
+from tests.integration_tests.hints import BoundPortedRuneRangesPair
+from tests.utils import equivalence
+from . import strategies
+
+
+@given(strategies.rune_ranges_pairs, strategies.rune_ranges_pairs)
+def test_basic(first_pair: BoundPortedRuneRangesPair,
+               second_pair: BoundPortedRuneRangesPair) -> None:
+    first_bound, first_ported = first_pair
+    second_bound, second_ported = second_pair
+
+    assert equivalence(first_bound == second_bound,
+                       first_ported == second_ported)
