931fcdcbd58d14919d9f9784d23571b2ef546d25
Azat Ibrakov <azatibrakov@gmail.com>
2021-11-12 18:08:31 +0300

    Complete ported 'Expression' class

--- rei/expression.py
+++ rei/expression.py
@@ -1,13 +1,27 @@
+from typing import Optional
+
 from reprit.base import generate_repr
+
+from .canned_option import CannedOption
+from .options import Options
 
 
 class Expression:
-    __slots__ = '_pattern',
+    __slots__ = '_options', '_pattern'
 
-    def __init__(self, pattern: str) -> None:
+    def __init__(self,
+                 pattern: str,
+                 options: Optional[Options] = None) -> None:
         self._pattern = pattern
+        self._options = (Options.from_canned_option(CannedOption.DEFAULT)
+                         if options is None
+                         else options)
 
     __repr__ = generate_repr(__init__)
+
+    @property
+    def options(self) -> Options:
+        return self._options
 
     @property
     def pattern(self) -> str:
