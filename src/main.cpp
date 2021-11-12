6db7b4e206b496ea925e6007406dd3b6c25efeba
Azat Ibrakov <azatibrakov@gmail.com>
2021-11-12 12:14:46 +0300

    Add bound 'CharClassBuilder.is_full' property

--- src/main.cpp
+++ src/main.cpp
@@ -376,7 +376,8 @@ PYBIND11_MODULE(MODULE_NAME, m) {
       .def("add_range",
            [](CharClassBuilder& self, const Rune& low, const Rune& high) {
              return self.AddRange(low.raw(), high.raw());
-           });
+           })
+      .def_property_readonly("is_full", &CharClassBuilder::full);
 
   py::enum_<Encoding>(m, ENCODING_NAME)
       .value("UTF_8", Encoding::EncodingUTF8)
